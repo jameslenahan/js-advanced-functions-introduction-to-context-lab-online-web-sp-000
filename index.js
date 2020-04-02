856880c3e24628ab34816ac9484d0d08b5e2cac7
James Lenahan <james.k.lenahan@gmail.com>
2020-04-02 20:58:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+let allWagesFor = function () {
+    let eligibleDates = this.timeInEvents.map(function (e) {
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function (memo, d) {
+        return memo + wagesEarnedOnDate.call(this, d)
+    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+
+    return payable
+}
+
+function findEmployeeByFirstName(allEmployees, firstName){
+    return allEmployees.find((employee) => {
+        return employee.firstName === firstName
+    })
+}
